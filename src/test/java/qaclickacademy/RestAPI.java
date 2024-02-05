107c72a92f4956198c3531439d444d50283d242b
Mayuri <patilmayu1808@gmail.com>
2024-02-05 11:50:03 +0530

    This is Indian Time Zone Guy

---
+++ src/test/java/qaclickacademy/RestAPI.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPI {
+	
+	@Test
+	public void PostJira() {
+		
+		System.out.println("Post Jira 1");
+		System.out.println("Post Jira 2");
+		System.out.println("Post Jira 3");
+		System.out.println("Post Jira 4");
+	}
+
+}
