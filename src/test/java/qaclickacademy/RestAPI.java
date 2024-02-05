45713563519f04c77f8eb0123d1ef76317158f50
Mayuri <patilmayu1808@gmail.com>
2024-02-05 13:39:41 +0530

    Asian Guy Commit

---
+++ src/test/java/qaclickacademy/RestAPI.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPI {
+	
+	@Test
+	public void PostJira() {
+		
+		System.out.println("Post Jira 1");
+		System.out.println("Post Jira 2");
+		System.out.println("Post Jira 3");
+		System.out.println("Post Jira 4");
+		System.out.println("Post Jira 5");
+		System.out.println("Post Jira 6");
+		System.out.println("Post Jira 7");
+		System.out.println("Post Jira 8");
+		System.out.println("Post Jira 9");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Selenium.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Selenium {
+	
+	@Test
+	public void BrowserAutomation() {
+		
+		
+		System.out.println("selenium Automation");
+		System.out.println("Web Automation ");
+	}
+
+}
